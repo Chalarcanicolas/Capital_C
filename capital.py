49558f238d82dd1e248c2230c2fb31251a44a0b2
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-03-31 06:21:39 -0500

    si

--- capital.py
+++ capital.py
@@ -1,3 +1,14 @@
-c = int(input("Digite el valor de su capital: "))
-suma = 0 
-                                                                                                                                        
+capital = float(input("Digite el valor de su capital: "))
+duplicado = capital + capital
+meses = 0
+interes = 0
+
+meses = 0
+
+while capital < duplicado:
+    interes = capital*0.5
+    capital = capital + interes
+    meses = meses + 1
+
+
+print("el capital se duplica en: " + str(meses) + " MESES ")
